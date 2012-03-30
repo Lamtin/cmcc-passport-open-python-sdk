68881685a9e43c9b6cbee209520d7bb904cf8f20
Lamtin <lamtin@qq.com>
2012-03-31 01:39:36 +0800

    add code

---
+++ falsk.py
@@ -0,0 +1,57 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+from flask import Flask, session, redirect, url_for, escape, request
+from cmcc import ChinaMobile
+
+app = Flask(__name__)
+
+conn = ChinaMobile()
+
+@app.route('/')
+def index():
+    if 'login' in session:
+        return redirect(url_for('auth'))
+
+    result = conn.get_request_token('http://127.0.0.1:5000/auth')
+    session['oauth_token_secret'] = result['oauth_token_secret']
+    session['oauth_token'] = result['oauth_token']
+    conn.set_token(result['oauth_token'])
+    return "<a href='%s'>Login</a>" % (conn.get_authorize_url() ,)
+
+@app.route('/auth')
+def auth():
+    oauth_verifier = request.args.get('oauth_verifier', None)
+    if oauth_verifier:
+        oauth_verifier = request.args.get('oauth_verifier')
+        conn.set_token_secret(session['oauth_token_secret'])
+        conn.set_token(session['oauth_token'])
+        result = conn.get_access_token(oauth_verifier)
+        session.pop('oauth_token_secret', None)
+        session.pop('oauth_token', None)
+        session['oauth_access_token_secret'] = result['oauth_token_secret']
+        session['oauth_access_token'] = result['oauth_token']
+        session['login'] = '1'
+        return redirect(url_for('auth'))
+
+    if 'login' not in session:
+        return redirect(url_for('index'))
+
+    conn.set_token(session['oauth_access_token'])
+    conn.set_token_secret(session['oauth_access_token_secret'])
+
+    user = conn.api_get('user/profile')
+
+    return "Nick Name: %s<br />Email: %s<br />Gender: %s<br /><a href='/logout'>Logout</a>" % (user['nick_name'], user['email'], user['gender'] )
+
+@app.route('/logout')
+def logout():
+    session.pop('oauth_access_token_secret', None)
+    session.pop('oauth_access_token', None)
+    session.pop('login', None)
+    return redirect(url_for('index'))
+
+
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+
+if __name__ == '__main__':
+    app.run(debug=True)
